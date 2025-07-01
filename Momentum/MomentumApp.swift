3ab1b4f33143de5be0c48f4edcad56e878132f75
Joel Kim <joelminchankim@gmail.com>
2025-07-01 12:25:46 -0400

    Initial commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by joel on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
